1d8ae3491c71328cec5e5eea0be4c7c255e60e88
Jonathan Drew <jonathan.drew@codeimmersives.com>
2019-08-06 12:37:57 -0400

    halfway

--- main.js
+++ main.js
@@ -1,54 +1,36 @@
 // Make your global array here.
 // Put a few todos in there to start with!
 // This is mostly for testing purposes.
+let addButton = document.querySelector(".add-todo");
+let whereYouTypetoAdd = document.querySelector(".todo-input");
+let removeButton = document.querySelector(".remove-todo");
+let whereYouTypeToRemove = document.querySelector(".index-input");
 const todos = [];
 
 // Rewrite your print todo function to put the todo on the dom instead of in the console.
 // Keep the same name so that your other function still calls it!
-function printTodo(todo) {
-  // Use `document.createElement` to make an li
-
-
-  // Put the ul that's already in our html file in a variable.
-
-
-  // Append the li we made to the ul as the last child.
-
-
+function printTodoToScreen(task) {
+  const todoList = document.querySelector(".todo-list");
+  let listItem = document.createElement("li");
+  listItem.innerText = task;
+  todoList.appendChild(listItem);
 }
-
-
-// Add an event listener to the ADD button to run a function that we'll write shortly that adds todos.
-
-
-// Now write the function that the event listener will run. It will take what's in the user input and add it to the todo list array.
-
-/*
-
-Here's what it'll do, in order. Feel free to put these comments in your function!
-
-
-
-// Place in a variable the node element for the add todo input box.
-
-// Place in a variable the text that the user typed into that input box. You can "dot off" the variable above to find a property that's on that element. If you're not sure if you have it, try console logging what you've got!
-
-  // Now put that todo string in your todo list. You have a function for that!
-  addTodo(userInput);
-
-  // Now use that print function to add the todo to the dom!
-  printTodo(userInput);
-
-  // Optionally but helpfully, set that input box's value back to an empty string to empty the box so you can type a different todo in there.
-  inputBox.value = '';
-
-*/
-
+addButton.addEventListener("click", function() {
+  addTodos(whereYouTypetoAdd.value);
+  printTodoToScreen(whereYouTypetoAdd.value);
+  whereYouTypetoAdd.value = "";
+}
+)
 
 // Add an event listener to the REMOVE button to run a function that we'll write shortly that removes todos.
 
-
 // Now write the function that the event listener will run. It will take what's in the user input and remove it from the todo list array.
+removeButton.addEventListener("click", function(){
+  removeTodos(whereYouTypeToRemove.value);
+  removeEntireList()
+  
+})
+
 
 /* Here's what it'll do, in order. Feel free to put these comments in your function!
 
@@ -64,6 +46,7 @@ Here's what it'll do, in order. Feel free to put these comments in your function
 */
 
 // Write a function that erases everything from the list.
+function removeEntireList() {}
 
 /*
 
@@ -83,24 +66,23 @@ Here's what it'll do, in order. Feel free to put these comments in your function
 // printList();
 // But whatever you named it, use those names!
 
-
 // A function that prints everything on our todo list, INDIVIDUALLY.
 // Make SURE to use the above function!
 function printList() {
   let i = 0;
   while (i < todos.length) {
-    printTodo(todos[i]);
+    printTodos(todos[i]);
     i = i + 1;
   }
 }
 
 // A function that adds an item to our todo list.
 // Where on the list should we add it?
-function addTodo(todo) {
-  todos.push(todo);
+function addTodos(task) {
+  todos.push(task);
 }
 
 // A function that removes an item at a given index from our todo list.
-function removeTodo(i) {
+function removeTodos(i) {
   todos.splice(i, 1);
 }
